b35c073aa25faa7579e4ef32a1dab4f23cb8aa10
Nimesh Bhalani <bhalaninimesh176@gmail.com>
2020-05-10 15:57:31 +0530

    => Face Id auth added

--- App.js
+++ App.js
@@ -1,19 +1,37 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import * as LocalAuthentication from "expo-local-authentication";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+export default class App extends React.Component {
+  state = {
+    auth_status: "",
+  };
+  componentDidMount = async () => {
+    this.setState((state) => ({ auth_status: "pending" }));
+    const result = await LocalAuthentication.authenticateAsync({
+      promptMessage: "Use your bio ID for sign in.",
+    });
+    console.log(result); //<-- {"error": "unknown", "message": "", "success": false}
+    if (result.success) {
+      this.setState((state) => ({ auth_status: "success" }));
+    } else {
+      this.setState((state) => ({ auth_status: "failed" }));
+    }
+  };
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>{this.state.auth_status}</Text>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
